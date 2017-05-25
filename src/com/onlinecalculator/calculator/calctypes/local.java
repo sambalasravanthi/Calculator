1b9f6f764f9758d182535a0015a3db16f29ac265
iSTAR <iSTAR@192.168.100.12>
2017-05-25 18:24:09 +0530

    local variable

---
+++ src/com/onlinecalculator/calculator/calctypes/local.java
@@ -0,0 +1,16 @@
+package com.onlinecalculator.calculator.calctypes;
+import java .util.*;
+public class local {
+static void display(){
+int x=10;
+Scanner sc =new Scanner(System.in);
+System.out.println("local variable x="+x);
+x=sc.nextInt();
+}
+
+	public static void main(String args[])
+	{
+		
+   display();   
+}
+}
